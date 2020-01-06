b4d48b5e64d5271f2720a67c5e190ffbc1152be5
Sumanta Kabiraj <sumantablog@gmail.com>
2020-01-06 16:39:46 +0530

    Add Bengali Lang in text.php

--- resources/lang/bn/text.php
+++ resources/lang/bn/text.php
@@ -1,7 +1,82 @@
 <?php
 return [
 
-
+    'home'=>'হোম',
+    'grievance'=>'অভিযোগ',
+    'grievance_status'=>'অভিযোগ স্থিতি',
+    'case_search'=>'মামলার সন্ধান',
+    'todays_hearing'=>"আজকের শ্রবণ",
+    'login'=>"প্রবেশ করুন",
+    
+    'dashboard'=>"ড্যাশবোর্ড",
+    'user'=>"ব্যবহারকারী",
+    'user_create'=>"ব্যবহারকারী তৈরি করুন",
+    'user_list'=>"ব্যবহারকারীর তালিকা",
+    'case'=>"মামলা",
+    'case_entry'=>"মামলার এন্ট্রি",
+    'case_list'=>"মামলার তালিকা",
+    'grievance'=>"অভিযোগ",
+    'recieved'=>"গৃহীত",
+    'forwarded'=>"ফরওয়ার্ড",
+    'resolved'=>"স্থিরপ্রতিজ্ঞ",
+    'closed'=>"বন্ধ",
+    'report'=>"প্রতিবেদন",
+    'pending_grievance_report'=>"মুলতুবি অভিযোগ",
+    'user_log_details'=>"লগ বিবরণ",
+    'logout'=>"প্রস্থান",
+    
+    //Label
+    'mobile_no'=>"মোবাইল নাম্বার :",
+    'name'=>"নাম :",
+    'designation'=>"উপাধি :",
+    'description'=>"বিবরণ :",
+    'case_number'=>"মামলার নম্বর :",
+    'next_hearing_date'=>"পরবর্তী শুনানির তারিখ :",
+    
+    'email'=>"ই-মেইল :",
+    'complain'=>"অভিযোগ করা :",
+    'attachment'=>"সংযুক্তি :",
+    'grievance_id'=>"অভিযোগ আইডি :",
+    
+    //Place Holder
+    
+    'enter_mobile_number'=>"মোবাইল নম্বর লিখুন",
+    'enter_name'=>"নাম লিখুন",
+    'enter_designation'=>"পদবি লিখুন",
+    'enter_description'=>"বিবরণ লিখুন",
+    
+    'enter_next_hearing_date'=>"পরবর্তী শুনানির তারিখ লিখুন",
+    'enter_case_number'=>"মামলার নম্বর লিখুন",
+    
+    'enter_email'=>"ই-মেইল লিখুন",
+    'enter_complain'=>"অভিযোগ লিখুন",
+    'enter_attachment'=>"সংযুক্তি লিখুন",
+    'enter_grievance_id'=>"অভিযোগ আইডি লিখুন",
+    'enter_captcha'=>"ক্যাপচা লিখুন",
+    
+    
+    //Button
+    
+    'sign_in'=>"সাইন ইন করুন ",
+    'save'=>"সংরক্ষণ",
+    'submit'=>"উপস্থাপিত করা ",
+    'search'=>"অনুসন্ধান করুন ",
+    
+    //admin panel
+    'welcome_to_admin_panel'=>"অ্যাডমিন প্যানেলে আপনাকে স্বাগতম ",
+    'malda_district_admin_portal'=>"মালদা জেলা প্রশাসন পোর্টাল",
+    
+    'total_grievances'=>"মোট অভিযোগ",
+    'pending_grievances'=>"মুলতুবি অভিযোগ",
+    'forwarded_grievances'=>"ফরোয়ার্ড অভিযোগ",
+    'closed_grievances'=>"বন্ধ অভিযোগ",
+    'resolved_grievances'=>"অভিযোগগুলি সমাধান করুন",
+    
+    //Table Heading
+    'recieved_table_heading'=>"অভিযোগের তালিকা প্রাপ্ত ",
+    'forwarded_table_heading'=>"ফরওয়ার্ড করা অভিযোগের তালিকা ",
+    'resolved_table_heading'=>"সমাধানের অভিযোগের তালিকা ",
+    'closed_table_heading'=>"বন্ধ অভিযোগের তালিকা",
 
 
 ];
