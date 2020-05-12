19077596a128568250f2f3888bc087c7776fc3ed
Arpan Ghosh <arpanghosh767@gmail.com>
2020-05-12 17:03:50 +0530

    Solved Unnecessary entries in audit trail Problem

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('language/{locale}', function ($locale) {
 
 
 
-Route::group(['middleware' => ['sessioncheking','disablepreventback','userlogdetails']],function(){
+Route::group(['middleware' => ['sessioncheking','disablepreventback']],function(){
     Auth::routes();
   Route::get('/index', 'UserController@index');
   Route::get('/userRegisration','LoginController@userRegisration');
@@ -39,123 +39,29 @@ Route::group(['middleware' => ['sessioncheking','disablepreventback','userlogdet
   })->name('log_details');
   //Route::get('/home', 'HomeController@index');
 });
-
-Route::group(['middleware' => 'userlogdetails'],function(){
   Route::get('/', 'UserController@home');
   Route::get('/login','LoginController@login');
-  Route::post('/login-action','LoginController@loginAction');
-
-  Route::post('/userRegistrationAction','LoginController@userRegistrationAction');
-
-
   Route::post('/userlist_datatable','registrationController@userlist_datatable');
-
-  Route::post('/user_edit','registrationController@user_edit');
-  Route::post('/user_delete','registrationController@user_delete');
-
-
-  Route::post('/logout','LoginController@logout');
-
-
-
-
-
-
-
-
-
-  /****************************SDO Court*******************/
-
-  Route::post('/save_case','SDOCourtController@save_case');
-
   Route::post('/caselist_datatable','SDOCourtController@caselist_datatable');
-  Route::post('/case_edit','SDOCourtController@case_edit');
-  Route::post('/case_delete','SDOCourtController@case_delete');
-
-
-
-
-  /*********************************************************/
-
-  Route::post('/save_otp','registrationController@save_otp');
-  Route::post('/check_otp','registrationController@check_otp');
-
-
-  /****************************Change Password*******************/
-  Route::get('/change_pin','LoginController@change_pin');
-  Route::post('/user_pin_change','LoginController@user_pin_change');
-  Route::get('/edit_profile','LoginController@edit_profile');
-  Route::post('/user_name_change','LoginController@user_name_change');
-
-
-
-  Route::get('/forgotPassword','registrationController@forgotPassword');
-
-  Route::post('/saveOtpForLogin','registrationController@saveOtpForLogin');
-  Route::post('/checkOtpForLogin','registrationController@checkOtpForLogin');
-  Route::post('/passwordChange','registrationController@passwordChange');
-
-
-
-
-
-  Route::get('/search_case', 'SDOCourtController@search_case');
   Route::post('refreshcaptcha', 'SDOCourtController@refreshCaptcha');
   Route::post('capchavalidation', 'SDOCourtController@capchavalidation');
-
-
-
-
-
-  //******************************Grivance List******************************//
+  Route::get('/grievance', 'GrievanceController@grivense');
   Route::post('/save_otp_for_grievance','GrievanceController@save_otp_for_grievance');
   Route::post('/check_otp_for_grievance','GrievanceController@check_otp_for_grievance');
-  Route::get('/grievance', 'GrievanceController@grivense');
-  Route::post('/grivanceSave', 'GrievanceController@grivanceSave');
-
   Route::post('grievance_datatable', 'GrievanceController@grievance_datatable');
-
   Route::post('view_user', 'GrievanceController@view_user');
   Route::post('user_list', 'GrievanceController@user_list');
-  Route::post('save_forword', 'GrievanceController@save_forword');
-
-  Route::post('forwored_grievance_datatable', 'GrievanceController@forwored_grievance_datatable');
-
   Route::post('view_user_for_forward', 'GrievanceController@view_user_for_forward');
-  Route::post('close_grievance', 'GrievanceController@close_grievance');
-
-  
   Route::post('closed_grievance_datatable', 'GrievanceController@closed_grievance_datatable');
-
-
-
-
-
-
   Route::get('session', function () {
     return view('layout.session');
   });
-
-
-
-
-  
-
-
-
   Route::get('/todays_hearing', 'SDOCourtController@todays_hearing');
-
   Route::post('/caselist_datatable_for_todays_hearing', 'SDOCourtController@caselist_datatable_for_todays_hearing');
-
-  //grievance Status
-
   Route::get('/grievance_status', 'GrievanceStatusController@grievance_status');
   Route::get('refreshcaptcha', 'SDOCourtController@refreshCaptcha');
-  Route::post('grievance_statuss', 'GrievanceStatusController@grievance_statuss');
-
   Route::post('/save_otp_for_grievancestatus','GrievanceStatusController@save_otp_for_grievancestatus')->name('save_otp_for_grievancestatus');
   Route::post('/check_otp_for_grievancestatus','GrievanceStatusController@check_otp_for_grievancestatus')->name('check_otp_for_grievancestatus');
-
 
   ////////Resolve
 
@@ -167,7 +73,43 @@ Route::group(['middleware' => 'userlogdetails'],function(){
  
   Route::post('pending_grievance_datatable', 'ReportController@pending_grievance_datatable');
   Route::post('show_pending_grievance', 'ReportController@show_pending_grievance');
+  Route::post('forwored_grievance_datatable', 'GrievanceController@forwored_grievance_datatable');
+
+Route::group(['middleware' => 'userlogdetails'],function(){
+  
+  Route::post('/login-action','LoginController@loginAction');
+  Route::post('/userRegistrationAction','LoginController@userRegistrationAction');
+  Route::post('/user_edit','registrationController@user_edit');
+  Route::post('/user_delete','registrationController@user_delete');
+  Route::post('/logout','LoginController@logout');
+
+  /****************************SDO Court*******************/
+
+  Route::post('/save_case','SDOCourtController@save_case');
+  Route::post('/case_edit','SDOCourtController@case_edit');
+  Route::post('/case_delete','SDOCourtController@case_delete');
 
 
+
+  /****************************Change Password*******************/
+ 
+
+  Route::post('/saveOtpForLogin','registrationController@saveOtpForLogin');
+  Route::post('/checkOtpForLogin','registrationController@checkOtpForLogin');
+
+  Route::get('/search_case', 'SDOCourtController@search_case');
+  
+
+  //******************************Grivance List******************************//
+  Route::post('/grivanceSave', 'GrievanceController@grivanceSave');
+  Route::post('save_forword', 'GrievanceController@save_forword');
+  Route::post('close_grievance', 'GrievanceController@close_grievance');
+
+  //grievance Status
+
+  
+  Route::post('grievance_statuss', 'GrievanceStatusController@grievance_statuss');
+
+ 
 
 });
