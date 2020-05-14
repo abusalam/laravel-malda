5a357ddcaade3eba3122add252f831cde0e2f0e2
Abu Salam Parvez Alam <abusalamparvezalam@gmail.com>
2020-05-14 11:37:05 +0530

    Updated Captcha Config

--- config/captcha.php
+++ config/captcha.php
@@ -3,24 +3,24 @@
 return [
     'characters' => ['2', '3', '4', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'j', 'm', 'n', 'p', 'q', 'r', 't', 'u', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'X', 'Y', 'Z'],
     'default'    => [
-        'length'  => 4,
+        'length'  => 6,
         'width'   => 240,
-        'height'  => 36,
+        'height'  => 72,
         'quality' => 90,
         'math'    => false,
     ],
     'math' => [
         'length'  => 9,
-        'width'   => 120,
-        'height'  => 36,
+        'width'   => 240,
+        'height'  => 72,
         'quality' => 90,
         'math'    => true,
     ],
 
     'flat' => [
         'length'     => 6,
-        'width'      => 160,
-        'height'     => 46,
+        'width'      => 240,
+        'height'     => 72,
         'quality'    => 90,
         'lines'      => 6,
         'bgImage'    => false,
@@ -30,13 +30,13 @@ return [
     ],
     'mini' => [
         'length' => 3,
-        'width'  => 60,
-        'height' => 32,
+        'width'  => 120,
+        'height' => 36,
     ],
     'inverse' => [
         'length'    => 5,
-        'width'     => 120,
-        'height'    => 36,
+        'width'     => 240,
+        'height'    => 72,
         'quality'   => 90,
         'sensitive' => true,
         'angle'     => 12,
